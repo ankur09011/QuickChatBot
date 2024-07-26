d88a2c6174f9a1c05e20903fab4244fd0e0c2f8b
Ankur Agrawal <ankur09011@gmail.com>
2024-07-26 17:34:15 +0000

    Intial setup commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # QuickChatBot
 A quick implementation of a intent chatbot
+
+
+Clone repo and create a virtual environment
+```
+$ git clone https://github.com/ankur09011/QuickChatBot.git
+$ cd QuickChatBot
+$ python3 -m venv venv
+$ . venv/bin/activate
+```
+
+Install dependencies
+
+```
+$ (venv) pip install Flask flask_cors torch torchvision nltk
+```
+
+## Note to intialise NLTK use externally, required only one time
+```
+import nltk
+nltk.download('punkt')
+```
+
+Modify intents.json with different intents and responses for your Chatbot
+
+```
+$ (venv) python train.py
+```
+
+This will dump chat_model.pth file. And then run the flask app, it will host endpoint at 5555
+
+```
+$ (venv) python app.py
+```
+
+### Use frontend folder to test
---
+++ app.py
@@ -0,0 +1,68 @@
+# create flask app
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import random
+import json
+
+import torch
+
+from model import RevChatNeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = None
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "chat_model.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = RevChatNeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Panda"
+
+
+def get_response(msg):
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
+
+    return "Can you please elaborate?"
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/predict', methods=['POST'])
+def predict_intecnt():
+    message = request.json['message']
+    response = get_response(message)
+    return jsonify({'answer': response})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555, host='0.0.0.0')
